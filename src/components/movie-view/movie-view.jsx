f2afa26241dd4ed845908f5bf11f0ec851ee8926
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-05-01 00:44:31 +0200

    apply Bootstrap to movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import {Card, Button} from "react-bootstrap";
+import {Card, Button, Container, Row, Col} from "react-bootstrap";
+import './movie-view.scss'
 
 
 export class MovieView extends React.Component {
 
     render(){
         const {movie, onClickBack}=this.props;
+
+        const hasBirth=movie.Director.Birth.toString().trim();
+
         return (
-            <Card className="card">
-                <Card.Img variant="top" src={movie.ImageURL}/>
-                <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
-                    <Button variant="primary" onClick={() => { onClickBack(); }}>Back</Button>
-                </Card.Body>
+            <Card className="movie-view">
+                    <Row >
+                        <Col xs={12} s={12} md={12} lg={6}>
+                            <Card.Img variant="top" src={movie.ImageURL}/>
+                        </Col>
+                        <Col className="card-body--container">
+                            <Card.Body className="card-body">
+                                <Card.Title>{movie.Title}</Card.Title>
+                                <Card.Text>{movie.Description}</Card.Text>
+                                <Card.Text><span className="bold">Genre: </span>{movie.Genre.Name}</Card.Text>
+                                <Card.Text>
+                                    <span className="bold">Director: </span>{movie.Director.Name} {hasBirth && `(born: ${movie.Director.Birth})`}
+                                </Card.Text>
+                                <Card.Text><span className="bold">Biography: </span>{movie.Director.Bio}</Card.Text>
+                                <Button variant="outline-dark" onClick={() => { onClickBack(); }}>Back</Button>
+                            </Card.Body>
+                        </Col>
+                    </Row>
+
             </Card>
         );
     }
