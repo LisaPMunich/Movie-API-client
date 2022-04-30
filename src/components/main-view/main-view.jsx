37be53941adecf6f9522dd77b90e9b5d747d3fba
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-05-01 00:43:52 +0200

    Apply Bootstrap to main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import Row from 'react-bootstrap/Row';
-import {Col} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
+import './main-view.scss'
+
 
 import {LoginView} from '../login-view/login-view';
 import {RegistrationView} from '../registration-view/registration-view';
@@ -62,18 +63,19 @@ export class MainView extends React.Component {
             return <div className="main-view"/>
         }
         return (
-            <Row className="main-view justify-content-md-center">
+            <Row className="rounded mt-4 mx-3">
                 {selectedMovie
                     ? (
-                        <Col md={8}>
-                            <MovieView movie={selectedMovie} onClickBack={newSelectedMovie => {
+                        <Col md={12}>
+                            <MovieView className="mx-auto" movie={selectedMovie} onClickBack={newSelectedMovie => {
                                 this.setSelectedMovie(newSelectedMovie);
                             }}/>
                         </Col>
                     )
                     : movies.map(movie => (
-                        <Col md={3}>
+                        <Col key={movie._id} xs={12} sm={6} md={4} lg={3} className="movie-card mb-4">
                             <MovieCard
+                                className="movie-card"
                                 key={movie._id}
                                 movie={movie}
                                 onMovieClick={(newSelectedMovie) => {
