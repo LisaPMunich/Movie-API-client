f678f3ccb8302a538d5c258caa757b5dcb322c2f
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-05-01 00:42:57 +0200

    Apply Bootstrap to Login View

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import {Button, Form} from 'react-bootstrap';
+import './login-view.scss';
 
 export function LoginView(props) {
     const [ name, setName ] = useState('');
@@ -13,21 +13,24 @@ export function LoginView(props) {
     };
 
     return (
-        <>
-            <Form>
-                <Form.Group controlId="formUsername">
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control type="text" onChange={e=>setName(e.target.value)}/>
-                </Form.Group>
-                <Form.Group controlId="formPassword">
-                    <Form.Label>Password:</Form.Label>
-                    <Form.Control type="password" onChange={e=>setPassword(e.target.value)}/>
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>Log In</Button>
-            </Form>
-            <Form>
-                <Button variant="secondary" type="submit">Register Here</Button>
-            </Form>
-        </>
+    <div className="login--form-container">
+        <Form className="login--form">
+            <h2 className="login--form-title">Login</h2>
+            <Form.Group className="form--group"controlId="formUsername">
+                <Form.Control className="form--input-field" type="text" placeholder="Username" onChange={e=>setName(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="form--group" controlId="formPassword">
+                <Form.Control className="form--input-field" type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
+            </Form.Group>
+            <div className="form--button-container">
+                <Button className="form--button-login" variant="info" type="submit" onClick={handleSubmit}>SIGN IN</Button>
+            </div>
+            <p className="register-call">
+                <span className="register-text">Don't have an account?&nbsp;
+                    <a className="register-link" type="submit">Sign up</a>
+                </span>
+            </p>
+        </Form>
+    </div>
     );
 }
